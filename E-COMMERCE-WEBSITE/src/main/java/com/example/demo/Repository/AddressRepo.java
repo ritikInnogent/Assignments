a71419654de33c8830758cf4149d59fe40eab8b9
ritikInnogent <ritik.kawadker@innogent.in>
2025-11-12 01:41:19 +0530

    Delete E-COMMERCE-WEBSITE directory

--- E-COMMERCE-WEBSITE/src/main/java/com/example/demo/Repository/AddressRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepo  extends JpaRepository<Address, Long> {
-}
--- E-COMMERCE-WEBSITE/src/main/java/com/example/demo/Repository/OrderItemRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
-}
